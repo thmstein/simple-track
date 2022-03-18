d182a7c818103595c6d985e65dc3e02aace137cf
thmstein <39192510+thmstein@users.noreply.github.com>
2022-03-18 16:12:19 +0000

    Add files via upload
    Updated wrapper.py file with the following key changes:
    - Parameters changed so that these are appropriate for Himawari data and paths pointing to Himawari data;
    - Analysis of GPM prior to tracking, generating total rainfall accumulation on same grid as Himawari data;
    - Inclusion of storm object first (x,y) location (update to object_tracking.py incoming);
    - Calling user_functions to produce a 4-panel figure (update to user_functions.py incoming);

--- wrapper.py
+++ wrapper.py
@@ -4,6 +4,85 @@ import datetime
 import os
 import user_functions
 
+from scipy import interpolate 
+
+##################################################################
+# WHAT PERIOD DO YOU WANT TO RUN TRACKING FOR?
+##################################################################
+
+startyear = 2021
+startmonth = 1
+startday = 1
+starthour = 0
+#startmin = 0
+
+totalhours = 24
+
+start_time = datetime.datetime(startyear,startmonth,startday,starthour,0,0,0)
+
+##################################################################
+# BRIEFLY LOAD FIRST HIMAWARI FILE FOR LATS AND LONS
+# NEEDED TO INTERPOLATE GPM
+##################################################################
+
+# NB squarelength is important for correlations later
+squarelength = 50. ## Size in pixels of individual squares to run fft for (dx,dy) displacement. Must divide (x,y) lengths of the array!
+
+IMAGES_DIR = './output/'
+DATA_DIR = './himawari/java/13/2021/01/01/'
+filelist = os.listdir(DATA_DIR)
+filelist = np.sort(filelist)
+firsttime=True
+var,file_ID,yearval,monthval,dayval,hourval,minval,xx,yy = user_functions.loadfile(DATA_DIR + filelist[0],firsttime)
+ylen=np.size(yy)/np.int(squarelength)
+xlen=np.size(xx)/np.int(squarelength)
+xmat,ymat = np.meshgrid(range(0,xlen*np.int(squarelength)),range(0,ylen*np.int(squarelength)))
+lat = yy[0:ylen*np.int(squarelength)]
+lon = xx[0:xlen*np.int(squarelength)]
+
+##################################################################
+# THE FOLLOWING GENERATES GPM RAINFALL ACCUMULATION FOR THE PERIOD OF INTEREST.
+# THIS CAN BE USED TO IDENTIFY REGIONS OF INTEREST, E.G. RAINFALL MAXIMUM, TO FOCUS ANIMATIONS.
+##################################################################
+
+GPM_DIR = './gpm/'
+IMAGES_DIR = './output/'
+filelist = os.listdir(GPM_DIR)
+filelist = np.sort(filelist)
+firsttime = True
+
+for nt in range(len(filelist)):
+	# Load new image
+	var,yearval,monthval,dayval,hourval,minval,xx,yy = user_functions.loadgpm(GPM_DIR + filelist[nt],firsttime)
+	now_time = datetime.datetime(yearval,monthval,dayval,hourval,minval,0,0)
+	delta_t = now_time - start_time
+	elap = 24*delta_t.days + delta_t.seconds/3600.
+	if elap > totalhours:
+	    break
+	if firsttime==True:
+	    totalrain = var/2.
+	    theLats = yy
+	    theLons = xx
+	    firsttime=False
+	else:
+	    totalrain = totalrain + var/2.	
+
+latmin=-15
+latmax=0
+lonmin=100
+lonmax=120
+
+inda = np.where((theLons>lonmin) & (theLons<lonmax))
+indb = np.where((theLats>latmin) & (theLats<latmax))
+newrain = totalrain[indb[0][0]:indb[0][-1]+1,inda[0][0]:inda[0][-1]+1]
+x, y = np.float32(np.meshgrid(theLons[inda], theLats[indb]))
+xhim, yhim = np.float32(np.meshgrid(lon, lat))
+
+himrain=interpolate.griddata((x.flatten(),y.flatten()),newrain.flatten() , (xhim,yhim),method='linear')
+
+#x, y = np.float32(np.meshgrid(theLons,theLats))
+user_functions.plot_gpm(xhim,yhim,himrain,latmin,latmax,lonmin,lonmax,IMAGES_DIR)
+
 ##################################################################
 # THE FOLLOWING PARAMETERS SHOULD BE CHANGED BASED ON THE DATA (RESOLUTION ETC.)
 ##################################################################
@@ -12,13 +91,12 @@ import user_functions
 # Example 1: Radar data 5-minutes apart with time stamp in filename, dt = 5
 # Example 2: Satellite brightness temperatures hourly with time stamp in filename, dt = 1
 # NB. When writing storms, (dx,dy) will have units PIXELS per TIME STEP (specified by dt), so already scaled by number of missing files
-dt = 5. 
-dt_tolerance = 15. # Maximum separation in time allowed between consecutive images
+dt = 20. 
+dt_tolerance = 60. # Maximum separation in time allowed between consecutive images
 
-under_t=False ## True = labelling areas *under* the threshold (e.g. brightness temperature), False = labelling areas *above* threshold (e.g. rainfall)
-threshold = 3. ## Threshold used to identify objects (with value of variable greater than this threshold)
+under_t=True ## True = labelling areas *under* the threshold (e.g. brightness temperature), False = labelling areas *above* threshold (e.g. rainfall)
+threshold = 233. ## Threshold used to identify objects (with value of variable greater than this threshold)
 minpixel = 4. ## Minimum object size in pixels
-squarelength = 100. ## Size in pixels of individual squares to run fft for (dx,dy) displacement. Must divide (x,y) lengths of the array!
 rafraction = 0.01 ## Minimum fractional cover of objects required for fft to obtain (dx,dy) displacement
 dd_tolerance = 3. # Maximum difference in displacement values between adjacent squares (to remove spurious values) - scaled by num_dt if necessary
 halopixel = 5. ## Radius of halo in pixels for orphan storms - big halo assumes storms may spawn "children" at a distance multiple pixels away
@@ -37,17 +115,6 @@ if flagplot or flagplottest:
         my_dpi_global = 150 # for rasterized plots, this controls the size and quality of the final plot
     elif plot_type == '.png':
         my_dpi_global = 300
-
-flaghtml = True ## For creating a HTML page with animated gifs of the objects of interest (e.g. largest #maxgifs objects)
-obj_num_est = 1000 ## Estimate maximum total number of unique storms to be tracked, so that array can be initialised. Alternatively, append array but might be less efficient?
-maxgifs = 3 ## Total number of animations to be created for HTML page.
-padxy = 10 ## Padding of images so that object is not on the edge of the image.
-padt = 2 # Number of images to be shown before object first appears and after object disappears
-poi_string = 'area' # "poi" = "property of interest"; poi_string = String of property to be used for selection of objects of interest CURRENT OPTIONS are 'area', 'extreme', and 'meanvar' MUST BE single words to work as file string?
-poi_increasing = True # (1) looking for maxima (0) looking for minima [assumed to be different to "under_t"
-HTML_DIR = './html/'
-if os.path.exists(HTML_DIR + poi_string) == False:
-	raise ValueError('You need to create the folder "' + poi_string + '" in the directory ' + HTML_DIR)
 
 ##################################################################
 # THE FOLLOWING PARAMETERS CAN BE CHANGED, BUT SHOULD NOT BE
@@ -78,19 +145,7 @@ halosq = halopixel**2
 # NOT ESSENTIAL
 ##################################################################
 
-label_method='Rainfall rate > ' + thr_str + 'mm/hr'    
-
-##################################################################
-# THE REMAINDER IS THE SET UP FOR THE EXAMPLE DATA
-# THIS SHOULD BE ADJUSTED RELEVANT TO THE USER DATA
-# !!!TEST: MAKE SURE xall AND yall DIVIDE IN squarelength!!!
-##################################################################
-
-xmat,ymat = np.meshgrid(range(-200,200),range(-150,150))
-xall = np.size(xmat,0) # Only used to check grid dimensions
-yall = np.size(xmat,1) # Only used to check grid dimensions
-if (np.fmod(xall,squarelength)!=0 or np.fmod(yall,squarelength)!=0):
-	raise ValueError('Your grid does not match a multiple of squares as defined by squarelength')
+label_method='Brightness Temperature < ' + thr_str + 'K'    
 
 #################################################################
 # For test data, try the following:
@@ -105,17 +160,8 @@ if (np.fmod(xall,squarelength)!=0 or np.fmod(yall,squarelength)!=0):
 #DATA_DIR = './data/'
 #IMAGES_DIR = './output/'
 #################################################################
-DATA_DIR = './data/'
-IMAGES_DIR = './output/'
 filelist = os.listdir(DATA_DIR)
 filelist = np.sort(filelist)
-if flaghtml:
-	poi_array = np.nan*np.zeros((len(filelist),obj_num_est))
-	poi_boxleft = np.nan*np.zeros((len(filelist),obj_num_est))
-	poi_boxright = np.nan*np.zeros((len(filelist),obj_num_est)) # boxleft+boxwidth
-	poi_boxup = np.nan*np.zeros((len(filelist),obj_num_est))
-	poi_boxdown = np.nan*np.zeros((len(filelist),obj_num_est)) # boxup-boxheight
-
 if doradar:
 	rarray=np.sqrt(xmat**2+ymat**2);
 	azarray=np.arctan(xmat/ymat);
@@ -129,17 +175,35 @@ OldData, OldLabels, oldvar, newvar, prev_time = [], [], [], [], []
 newwas = 1
 plot_vectors = False
 
-start_time = datetime.datetime(2012,8,25,14,5,0,0)
 oldhourval = []
 oldminval = []
 oldmask = []
 newmask = []
 num_dt = []
+firsttime = True
 
 for nt in range(len(filelist)):
 	# Load new image
-	now_time = start_time + datetime.timedelta(seconds=300.*nt)
-	var,file_ID,hourval,minval = user_functions.loadfile(DATA_DIR + filelist[nt])
+	var,file_ID,yearval,monthval,dayval,hourval,minval,xx,yy = user_functions.loadfile(DATA_DIR + filelist[nt],firsttime)
+	now_time = datetime.datetime(yearval,monthval,dayval,hourval,minval,0,0)
+	delta_t = now_time - start_time
+	elap = 24*delta_t.days + delta_t.seconds/3600.
+	if elap > totalhours:
+	    break
+	if firsttime==True:
+	    ylen=np.size(yy)/np.int(squarelength)
+	    xlen=np.size(xx)/np.int(squarelength)
+	    xmat,ymat = np.meshgrid(range(0,xlen*np.int(squarelength)),range(0,ylen*np.int(squarelength)))
+	    lat = yy[0:ylen*np.int(squarelength)]
+	    lon = xx[0:xlen*np.int(squarelength)]
+	    #var=var[0:xlen*np.int(squarelength),0:ylen*np.int(squarelength)]
+	    #print(np.shape(xmat))
+	    #print(np.shape(ymat))
+	    #print(np.shape(var))
+	var=var[0:ylen*np.int(squarelength),0:xlen*np.int(squarelength)]
+	hourval=float(hourval)
+	minval=float(minval)
+	firsttime=False
 	print(file_ID)
 	write_file_ID = 'S' + sql_str + '_T'+ thr_str +'_A'+ areastr +'_'+ file_ID
 	NewLabels=object_tracking.label_storms(var,minpixel,threshold,struct2d,under_t)
@@ -171,10 +235,18 @@ for nt in range(len(filelist)):
 		object_tracking.write_storms(write_file_ID, start_time, now_time, label_method, squarelength, rafraction, newwas, NewData, doradar, misval, IMAGES_DIR)
 	# Plot tracked storm information (see user_functions.plot_example)
 	if flagplot:
-		user_functions.plot_example(write_file_ID, nt, var, xmat, ymat, newumat, newvmat, num_dt, wasarray, lifearray, threshold, IMAGES_DIR, plot_vectors)
-	# Store ID labels and track property of interest information
-	if flaghtml:
-		poi_array, poi_boxleft, poi_boxright, poi_boxup, poi_boxdown = object_tracking.write_html_files(file_ID, HTML_DIR, nt, wasarray, NewData, poi_array, poi_boxleft, poi_boxright, poi_boxup, poi_boxdown, poi_string)
+		#user_functions.plot_example(write_file_ID, nt, var, lon, lat, newumat, newvmat, num_dt, wasarray, lifearray, x0array, y0array, threshold, IMAGES_DIR, plot_vectors)
+		numstorms = NewLabels.max()
+		x0array=[]
+		y0array=[]
+		x1array=[]
+		y1array=[]
+		for sij in range(numstorms):
+			x0array.append(lon[np.int(NewData[sij].centroidx0)])
+			x1array.append(lon[np.int(NewData[sij].centroidx)])
+			y0array.append(lat[np.int(NewData[sij].centroidy0)])
+			y1array.append(lat[np.int(NewData[sij].centroidy)])
+		user_functions.plot_spisea(write_file_ID, nt, var, lon, lat, newumat, newvmat, num_dt, wasarray, lifearray, x0array, y0array, x1array, y1array, threshold, xhim, yhim, himrain, latmin,latmax,lonmin,lonmax, IMAGES_DIR, plot_vectors)
 	# Save tracking information in preparation for next image
 	OldData = NewData
 	OldLabels = NewLabels
@@ -183,5 +255,7 @@ for nt in range(len(filelist)):
 	oldminval = minval
 	plot_vectors = True
 
-if flaghtml:
-	user_functions.plot_poi(HTML_DIR, DATA_DIR, filelist, poi_array, poi_boxdown, poi_boxleft, poi_boxup, poi_boxright, maxgifs, padt, padxy, poi_increasing, poi_string, xmat, ymat)
+##################################################################
+# TRACKING IS NOW FINISHED.
+##################################################################
+
